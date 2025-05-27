181cbaa924b2fa789bfd42be053b105a3d3bce2f
sahasweety <149262417+sahasweety@users.noreply.github.com>
2025-05-27 20:23:41 +0530

    Update screenshot app.py

--- screenshot app.py
+++ screenshot app.py
@@ -3,7 +3,7 @@ import pyautogui
 
 def screenshot():
     name = int(round(time.time() * 1000))
-    name = 'C:/Users/Aaditya/OneDrive/Desktop/Python/python/.ipynb_checkpoints/screenshot data{}.png'.format(name)
+    name = 'C:/Users/Aaditya/OneDrive/Desktop/Python/python/screenshot data/{}.png'.format(name)
     time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
